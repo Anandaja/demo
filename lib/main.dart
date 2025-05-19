bcee96ac9342c037c9e41c26c34b8e84dd2d8752
Anandaja <anandajayan2003@gmail.com>
2025-05-19 12:47:33 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+final ButtonStyle style=ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 30,inherit: true),
+backgroundColor: Colors.green,
+foregroundColor: Colors.white);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Demo APP'),centerTitle: true,),
+        body:SafeArea(child: 
+         Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+                         ElevatedButton(
+               style: style,
+                  child: const Text('Hello DQ'),
+                  onPressed: () {
+                    print('Hello .. DQ');
+                  },
+                ),
+                const SizedBox(height: 30,),
+                           ElevatedButton(
+               style: style,
+                  child: const Text('Hello FAFA'),
+                  onPressed: () {
+                    print('Hello ....FAFA');
+                  },
+                ),
+                  const SizedBox(height: 30),
+                              ElevatedButton(
+                style: style,
+                child: const Text('Hello Rambo'),
+                onPressed: () {
+                  print('Hello ...Rambo');
+                },
+              ),
+            ],
+          ),
+      
+        ),)
+      ),
+    );
+  }
+}
